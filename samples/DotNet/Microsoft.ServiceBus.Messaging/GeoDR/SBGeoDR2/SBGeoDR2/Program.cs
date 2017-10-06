8323905bae9fd68149dbf45566fa7ce53bef8ea7
Sean Feldman <feldman.sean@gmail.com>
2017-10-05 23:29:29 -0600

    removing thread based code in favor of fully async code + small refactoring

--- samples/DotNet/Microsoft.ServiceBus.Messaging/GeoDR/SBGeoDR2/SBGeoDR2/Program.cs
+++ samples/DotNet/Microsoft.ServiceBus.Messaging/GeoDR/SBGeoDR2/SBGeoDR2/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using Microsoft.Azure.Management.ServiceBus;
@@ -11,13 +8,13 @@ using Microsoft.Rest;
 
 namespace SBGeoDR2
 {
-    class Program
+    static class Program
     {
-        public static string subscriptionId = "your subscription id"; // Pick existing subscription
-        public static string resourceGroupName = "your resource group name"; // Pick existing resource group       
+        static string subscriptionId = "your subscription id"; // Pick existing subscription
+        static string resourceGroupName = "your resource group name"; // Pick existing resource group       
 
-        public static string activeDirectoryAuthority = "https://login.microsoftonline.com";
-        public static string resourceManagerUrl = "https://management.azure.com/";
+        static string activeDirectoryAuthority = "https://login.microsoftonline.com";
+        static string resourceManagerUrl = "https://management.azure.com/";
 
         // Use the following link to learn how to setup an client app in access Active Directory
         // and grant that app rights to your azure subscription
@@ -25,40 +22,46 @@ namespace SBGeoDR2
         // Respectively follow this: https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-create-service-principal-portal
         // To get the below three values. Make sure to add the application as owner in your resource group via "Access control (IAM)".
 
-        public static string tenantId = "your tenant / directory id";     // Directory ID in portal
-        public static string clientId = "your client / application id";    //Application ID in portal
-        public static string clientSecrets = "your clientSecrets / key"; // Key in portal
+        static string tenantId = "your tenant / directory id";     // Directory ID in portal
+        static string clientId = "your client / application id";    //Application ID in portal
+        static string clientSecret = "your clientSecret / key"; // Key in portal
 
         
         static string geoDRPrimaryNS = "your primary ns";
         static string geoDRSecondaryNS = "your secondary ns";
-        static string alias = "your alias";        
+        static string alias = "your alias";
 
         static void Main(string[] args)
         {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
             // https://msdn.microsoft.com/en-us/library/azure/dn790557.aspx#bk_portal
-            string token = GetAuthorizationHeader();
+            string token = await GetAuthorizationHeaderAsync().ConfigureAwait(false);
 
             TokenCredentials creds = new TokenCredentials(token);
             ServiceBusManagementClient client = new ServiceBusManagementClient(creds) { SubscriptionId = subscriptionId };
             
             //// 1. Create Primary Namespace (optional)            
-            var namespaceParams = new SBNamespace()
+            var namespaceParams = new SBNamespace
             {
                 Location = "South Central US",
-                Sku = new SBSku()
+                Sku = new SBSku
                 {                    
                     Name = SkuName.Premium,
                     Capacity = 1
                 }
             };
-            var namespace1 = client.Namespaces.CreateOrUpdate(resourceGroupName, geoDRPrimaryNS, namespaceParams);
+            var namespace1 = await client.Namespaces.CreateOrUpdateAsync(resourceGroupName, geoDRPrimaryNS, namespaceParams)
+                .ConfigureAwait(false);
 
             //// 2. Create Secondary Namespace (optional if you already have an empty namespace available)
-            var namespaceParams2 = new SBNamespace()
+            var namespaceParams2 = new SBNamespace
             {
                 Location = "North Central US",
-                Sku = new SBSku()
+                Sku = new SBSku
                 {
                     Name = SkuName.Premium,
                     Capacity = 1
@@ -67,14 +70,16 @@ namespace SBGeoDR2
 
             //// If you re-run this program while namespaces are still paired this operation will fail with a bad request.
             //// this is because we block all updates on secondary namespaces once it is paired
-            var namespace2 = client.Namespaces.CreateOrUpdate(resourceGroupName, geoDRSecondaryNS, namespaceParams2);
+            var namespace2 = await client.Namespaces.CreateOrUpdateAsync(resourceGroupName, geoDRSecondaryNS, namespaceParams2)
+                .ConfigureAwait(false);
 
             // 3. Pair the namespaces to enable DR.            
-            ArmDisasterRecovery drStatus = client.DisasterRecoveryConfigs.CreateOrUpdate(
+            ArmDisasterRecovery drStatus = await client.DisasterRecoveryConfigs.CreateOrUpdateAsync(
                 resourceGroupName,
                 geoDRPrimaryNS,
                 alias,
-                new ArmDisasterRecovery { PartnerNamespace = geoDRSecondaryNS });
+                new ArmDisasterRecovery { PartnerNamespace = geoDRSecondaryNS })
+                .ConfigureAwait(false);
 
             while (drStatus.ProvisioningState != ProvisioningStateDR.Succeeded)
             {
@@ -88,11 +93,13 @@ namespace SBGeoDR2
                 Thread.CurrentThread.Join(TimeSpan.FromSeconds(30));
             }                        
 
-            client.Topics.CreateOrUpdate(resourceGroupName, geoDRPrimaryNS, "myTopic", new SBTopic());
-            client.Subscriptions.CreateOrUpdate(resourceGroupName, geoDRPrimaryNS, "myTopic", "myTopic-Sub1", new SBSubscription());
+            await client.Topics.CreateOrUpdateAsync(resourceGroupName, geoDRPrimaryNS, "myTopic", new SBTopic())
+                .ConfigureAwait(false);
+            await client.Subscriptions.CreateOrUpdateAsync(resourceGroupName, geoDRPrimaryNS, "myTopic", "myTopic-Sub1", new SBSubscription())
+                .ConfigureAwait(false);
 
             // sleeping to allow metadata to sync across primary and secondary
-            Thread.Sleep(1000 * 60);
+            await Task.Delay(TimeSpan.FromSeconds(60));
             
             // 6. Failover. Note that this Failover operations is ALWAYS run against the secondary ( because primary might be down at time of failover )
             // client.DisasterRecoveryConfigs.FailOver(resourceGroupName, geoDRSecondaryNS, alias);            
@@ -107,31 +114,21 @@ namespace SBGeoDR2
             // client.DisasterRecoveryConfigs.Delete(resourceGroupName, geoDRPrimaryNS, alias);
         }
 
-        private static string GetAuthorizationHeader()
+        private static async Task<string> GetAuthorizationHeaderAsync()
         {
-            AuthenticationResult result = null;
+            var context = new AuthenticationContext($"{activeDirectoryAuthority}/{tenantId}");
 
-            var context = new AuthenticationContext(string.Format("{0}/{1}", activeDirectoryAuthority, tenantId));
-
-            var thread = new Thread(() =>
-            {
-                result = context.AcquireTokenAsync(
-                    resourceManagerUrl,
-                    new ClientCredential(clientId, clientSecrets)).Result;
-            });
-
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Name = "AquireTokenThread";
-            thread.Start();
-            thread.Join();
+            var result = await context.AcquireTokenAsync(
+                resourceManagerUrl,
+                new ClientCredential(clientId, clientSecret))
+                .ConfigureAwait(false);
 
             if (result == null)
             {
-                throw new InvalidOperationException("Failed to obtain the JWT token");
+                throw new InvalidOperationException("Failed to obtain the JWT token.");
             }
 
-            string token = result.AccessToken;
-            return token;
+            return result.AccessToken;
         }
     }
 }
