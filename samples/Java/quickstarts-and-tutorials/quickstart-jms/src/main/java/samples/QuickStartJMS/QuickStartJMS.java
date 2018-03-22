abc6f028fa5641527af4f2513b84295cd7a5038f
Christian Wolf <chwolf@microsoft.com>
2018-03-21 20:05:24 -0700

    Adding new JMS quickstart

---
+++ samples/Java/quickstarts-and-tutorials/quickstart-jms/src/main/java/samples/QuickStartJMS/QuickStartJMS.java
@@ -0,0 +1,140 @@
+package samples.QuickStartJMS;
+
+import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
+import org.apache.commons.cli.*;
+import org.apache.log4j.*;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.util.Hashtable;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class QuickStartJMS {
+
+	 // Number of messages to send
+    private static int totalSend = 10;
+    //Tracking counter for how many messages have been received; used as termination condition
+    private static AtomicInteger totalReceived = new AtomicInteger(0);
+    
+    private static Logger logger = Logger.getRootLogger();
+    
+    String ConnectionString = null;
+    String QueueName = null;
+
+    public void run() throws Exception {
+
+        // The connection string builder is the only part of the azure-servicebus SDK library
+        // we use in this JMS sample and for the purpose of robustly parsing the Service Bus 
+        // connection string. 
+        ConnectionStringBuilder csb = new ConnectionStringBuilder(ConnectionString);
+        
+        // set up JNDI context
+        Hashtable<String, String> hashtable = new Hashtable<>();
+        hashtable.put("connectionfactory.SBCF", "amqps://" + csb.getEndpoint().getHost() + "?amqp.idleTimeout=120000&amqp.traceFrames=true");
+        hashtable.put("queue.QUEUE", QueueName);
+        //hashtable.put("queue.QUEUE", "testqueue");
+        hashtable.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.qpid.jms.jndi.JmsInitialContextFactory");
+        Context context = new InitialContext(hashtable);
+        ConnectionFactory cf = (ConnectionFactory) context.lookup("SBCF");
+        
+        // Look up queue
+        Destination queue = (Destination) context.lookup("QUEUE");
+
+        // we create a scope here so we can use the same set of local variables cleanly 
+        // again to show the receive side separately with minimal clutter
+        {
+            // Create Connection
+            Connection connection = cf.createConnection(csb.getSasKeyName(), csb.getSasKey());
+            // Create Session, no transaction, client ack
+            Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+            // Create producer
+            MessageProducer producer = session.createProducer(queue);
+
+            // Send messages
+            for (int i = 0; i < totalSend; i++) {
+                BytesMessage message = session.createBytesMessage();
+                message.writeBytes(String.valueOf(i).getBytes());
+                producer.send(message);
+                System.out.printf("Sent message %d.\n", i + 1);
+            }
+
+            producer.close();
+            session.close();
+            connection.stop();
+            connection.close();
+        }
+
+        {
+            // Create Connection
+            Connection connection = cf.createConnection(csb.getSasKeyName(), csb.getSasKey());
+            connection.start();
+            // Create Session, no transaction, client ack
+            Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+            // Create consumer
+            MessageConsumer consumer = session.createConsumer(queue);
+            // create a listener callback to receive the messages
+            consumer.setMessageListener(message -> {
+                try {
+                    // receives message is passed to callback
+                    System.out.printf("Received message %d with sq#: %s\n",
+                            totalReceived.incrementAndGet(), // increments the tracking counter
+                            message.getJMSMessageID());
+                    message.acknowledge();
+                } catch (Exception e) {
+                    logger.error(e);
+                }
+            });
+
+            // wait on the main thread until all sent messages have been received
+            while (totalReceived.get() < totalSend) {
+                Thread.sleep(1000);
+            }
+            consumer.close();
+            session.close();
+            connection.stop();
+            connection.close();
+        }
+
+        System.out.printf("Received all messages, exiting the sample.\n");
+        System.out.printf("Closing queue client.\n");
+    }
+
+    public static void main(String[] args) {
+
+    	QuickStartJMS app = new QuickStartJMS();
+        try {
+            app.runApp(args);
+            app.run();
+        } catch (Exception e) {
+            System.out.printf("%s", e.toString());
+        }
+        System.exit(0);
+    }
+
+    static final String SB_SAMPLES_CONNECTIONSTRING = "SB_SAMPLES_CONNECTIONSTRING";
+
+    public void runApp(String[] args) {
+    	 try {
+             // parse connection string from command line
+             Options options = new Options();
+             options.addOption(new Option("c", true, "Connection string"));
+             options.addOption(new Option("q", true, "Queue Name"));
+             CommandLineParser clp = new DefaultParser();
+             CommandLine cl = clp.parse(options, args);
+             if (cl.getOptionValue("c") != null && cl.getOptionValue("q") != null) {
+                 ConnectionString = cl.getOptionValue("c");
+                 QueueName =  cl.getOptionValue("q");
+             }
+             else
+             {
+                 HelpFormatter formatter = new HelpFormatter();
+                 formatter.printHelp("run jar with", "", options, "", true);
+             }
+         } catch (Exception e) {
+             System.out.printf("%s", e.toString());
+         }
+    }
+	
+}
