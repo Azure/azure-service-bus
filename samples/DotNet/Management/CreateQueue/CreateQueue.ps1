8aaccfa46dbf9964d5da7805cf1ee0e7b6e1af45
REDMOND\chwolf <chwolf@microsoft.com>
2018-03-27 17:04:35 -0700

    Adding create queue sample for cli and ps

---
+++ samples/DotNet/Management/CreateQueue/CreateQueue.ps1
@@ -0,0 +1,19 @@
+Login-AzureRmAccount
+
+Select-AzureRmSubscription -SubscriptionName "MyAzureSubName" 
+Get-AzureRmContext
+
+# Create a resource group 
+New-AzureRmResourceGroup –Name my-resourcegroup –Location westus2
+
+# Create a Messaging namespace
+New-AzureRmServiceBusNamespace -ResourceGroupName my-resourcegroup -NamespaceName namespace-name -Location westus2
+
+# Create a queue 
+New-AzureRmServiceBusQueue -ResourceGroupName my-resourcegroup -NamespaceName namespace-name -Name queue-name -EnablePartitioning $False
+
+# Get primary connection string (required in next step)
+Get-AzureRmServiceBusKey -ResourceGroupName my-resourcegroup -Namespace namespace-name -Name RootManageSharedAccessKey
+
+# Delete the created resources if desired
+Remove-AzureRmResourceGroup -Name my-resourcegroup
