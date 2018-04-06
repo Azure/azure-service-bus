a0fd8905ee0bed5436a45c914ad5488ddc93a2b8
Christian Wolf <chwolf@microsoft.com>
2018-04-05 18:13:54 -0700

    Adding tutorial

---
+++ samples/DotNet/GettingStarted/BasicSendReceiveTutorialwithFilters/BasicSendReceiveTutorialWithFilters/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicSendReceiveTutorialWithFilters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
