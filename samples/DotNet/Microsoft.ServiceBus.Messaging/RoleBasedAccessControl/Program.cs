10d37de5ff501f8a9b5bd0a089882dfc442f4a26
Ashish Chhabria <aschhab@microsoft.com>
2019-06-25 22:47:48 -0700

    fixing compilation errors on the various MSI login scenarios

--- samples/DotNet/Microsoft.ServiceBus.Messaging/RoleBasedAccessControl/Program.cs
+++ samples/DotNet/Microsoft.ServiceBus.Messaging/RoleBasedAccessControl/Program.cs
@@ -17,19 +17,20 @@
 
 namespace MessagingSamples
 {
+    using Microsoft.IdentityModel.Clients.ActiveDirectory;
+    using Newtonsoft.Json;
     using System;
+    using System.Collections.Generic;
+    using System.Configuration;
     using System.IO;
+    using System.Security.Cryptography.X509Certificates;
     using System.Text;
     using System.Threading.Tasks;
-    using Microsoft.ServiceBus;
-    using Microsoft.ServiceBus.Messaging;
-    using Newtonsoft.Json;
-    using System.Configuration;
-    using System.Security.Cryptography.X509Certificates;
-    using System.Collections.Generic;
-    using Microsoft.IdentityModel.Clients.ActiveDirectory;
-    using System.ServiceModel.Activation;
-    using Microsoft.IdentityModel.Tokens;
+    using Microsoft.Identity.Client;
+    using System.Collections;
+    using Microsoft.Azure.ServiceBus.Primitives;
+    using Microsoft.Azure.ServiceBus;
+    using Microsoft.Azure.ServiceBus.InteropExtensions;
 
     public class Program
     {
@@ -46,7 +47,7 @@ namespace MessagingSamples
             Console.WriteLine("Pick a scenario to run and hit ENTER:");
             Console.WriteLine("1) ManagedServiceIdentity (must run in an Azure VM or Web Job)");
             Console.WriteLine("2) Interactive User Login");
-            Console.WriteLine("3) Username and password Login");
+            Console.WriteLine("3) Client secret Login");
             Console.WriteLine("4) Client credential X.509 certificate");
 
             int option;
@@ -62,17 +63,17 @@ namespace MessagingSamples
                         await UserInteractiveLoginScenario();
                         break;
                     case 3:
-                        await UserPasswordCredentialScenario();
+                        await ClientCredentialsScenarioAsync();
                         break;
                     case 4:
                         await ClientCredentialsCertScenario();
                         break;
-                        
-                // Still pending
-                // Data owner scenario
-                // Custom role scenario
-                // Connection string scenario
-                // Control plane &data plane mixed scenario
+
+                        // Still pending
+                        // Data owner scenario
+                        // Custom role scenario
+                        // Connection string scenario
+                        // Control plane &data plane mixed scenario
 
                 }
             }
@@ -80,100 +81,153 @@ namespace MessagingSamples
 
         async Task ManagedServiceIdentityScenario()
         {
-            MessagingFactorySettings messagingFactorySettings = new MessagingFactorySettings
+            var qc = QueueClient.CreateWithManagedIdentity(new Uri($"sb://{ServiceBusNamespace}/").ToString(), QueueName);
+
+            await SendReceiveAsync(qc);
+
+        }
+
+        async Task SendReceiveAsync(QueueClient qc)
+        {
+
+            this.receiveClient = qc;
+            this.InitializeReceiver();
+
+            this.sendClient = qc;
+            var sendTask = this.SendMessagesAsync();
+
+            Console.ReadKey();
+
+            // shut down the receiver, which will stop the OnMessageAsync loop
+            await this.receiveClient.CloseAsync();
+
+            // wait for send work to complete if required
+            await sendTask;
+
+            await this.sendClient.CloseAsync();
+
+        }
+
+        async Task SendMessagesAsync()
+        {
+            dynamic data = new[]
             {
-                TokenProvider = TokenProvider.CreateManagedIdentityTokenProvider(),
-                TransportType = TransportType.Amqp
+                new {name = "Einstein", firstName = "Albert"},
+                new {name = "Heisenberg", firstName = "Werner"},
+                new {name = "Curie", firstName = "Marie"},
+                new {name = "Hawking", firstName = "Steven"},
+                new {name = "Newton", firstName = "Isaac"},
+                new {name = "Bohr", firstName = "Niels"},
+                new {name = "Faraday", firstName = "Michael"},
+                new {name = "Galilei", firstName = "Galileo"},
+                new {name = "Kepler", firstName = "Johannes"},
+                new {name = "Kopernikus", firstName = "Nikolaus"}
             };
 
-            await SendReceive(messagingFactorySettings);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                var message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data[i])))
+                {
+                    ContentType = "application/json",
+                    Label = "Scientist",
+                    MessageId = i.ToString(),
+                    TimeToLive = TimeSpan.FromMinutes(2)
+                };
+
+                await this.sendClient.SendAsync(message);
+                lock (Console.Out)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Message sent: Id = {0}", message.MessageId);
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        void InitializeReceiver()
+        {
+            // register the OnMessageAsync callback
+            this.receiveClient.RegisterMessageHandler(
+                async (message, token) =>
+                {
+                    if (message.Label != null &&
+                        message.ContentType != null &&
+                        message.Label.Equals("Scientist", StringComparison.InvariantCultureIgnoreCase) &&
+                        message.ContentType.Equals("application/json", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        var body = message.GetBody<Stream>();
+
+                        dynamic scientist = JsonConvert.DeserializeObject(new StreamReader(body, true).ReadToEnd());
+
+                        lock (Console.Out)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine(
+                                "\t\t\t\tMessage received: \n\t\t\t\t\t\tMessageId = {0}, \n\t\t\t\t\t\tSequenceNumber = {1}, \n\t\t\t\t\t\tEnqueuedTimeUtc = {2}," +
+                                "\n\t\t\t\t\t\tExpiresAtUtc = {5}, \n\t\t\t\t\t\tContentType = \"{3}\", \n\t\t\t\t\t\tSize = {4},  \n\t\t\t\t\t\tContent: [ firstName = {6}, name = {7} ]",
+                                message.MessageId,
+                                message.SystemProperties.SequenceNumber,
+                                message.SystemProperties.EnqueuedTimeUtc,
+                                message.ContentType,
+                                message.Size,
+                                message.ExpiresAtUtc,
+                                scientist.firstName,
+                                scientist.name);
+                            Console.ResetColor();
+                        }
+                    }
+                    await this.receiveClient.CompleteAsync(message.SystemProperties.LockToken);
+                },
+                new MessageHandlerOptions((eventargs) => { return Task.CompletedTask; })
+                {
+                    AutoComplete = false,
+                    MaxConcurrentCalls = 1
+                });
         }
 
         async Task UserInteractiveLoginScenario()
         {
-            MessagingFactorySettings messagingFactorySettings = new MessagingFactorySettings
-            {
-                TokenProvider = TokenProvider.CreateAadTokenProvider(
-                    new AuthenticationContext($"https://login.windows.net/{TenantId}"),
-                    ClientId,
-                    new Uri(ConfigurationManager.AppSettings["redirectURI"]),
-                    new PlatformParameters(PromptBehavior.SelectAccount),
-                    ServiceAudience.ServiceBusAudience
-                ),
-                TransportType = TransportType.Amqp
-            };
+            var qClient = QueueClient.CreateWithAzureActiveDirectory(
+                new Uri($"sb://{ServiceBusNamespace}").ToString(),
+                QueueName,
+                async (audience, authority, state) =>
+                {
+                    var app = PublicClientApplicationBuilder.Create(ClientId)
+                                .WithRedirectUri(ConfigurationManager.AppSettings["redirectURI"])
+                                .Build();
 
-            await SendReceive(messagingFactorySettings);
+                    var authResult = await app.AcquireTokenInteractive(new string[] { $"{audience}/.default" }).ExecuteAsync();
+
+                    return authResult.AccessToken;
+                },
+                $"https://login.windows.net/{TenantId}");
+
+            await SendReceiveAsync(qClient);
+
         }
 
-        static AzureActiveDirectoryTokenProvider.AuthenticationCallback UserInteractiveLoginCallback()
+        async Task ClientCredentialsScenarioAsync()
         {
-            return async (audience, authority, state) =>
-            {
-                var authContext = new AuthenticationContext($"https://login.windows.net/{TenantId}");
-                var clientId = ClientId;
-                var uri = new Uri(ConfigurationManager.AppSettings["redirectURI"]);
-                var platformParams = new PlatformParameters(PromptBehavior.SelectAccount);
+            var qClient = QueueClient.CreateWithAzureActiveDirectory(
+                new Uri($"sb://{ServiceBusNamespace}").ToString(),
+                QueueName,
+                async (audience, authority, state) =>
+                {
+                    IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(ClientId)
+                                    .WithAuthority(authority)
+                                    .WithClientSecret(ConfigurationManager.AppSettings["clientSecret"])
+                                    .Build();
 
-                var authResult = await authContext.AcquireTokenAsync(ServiceBusAudience.toString(), ClientId, uri, platformParams, ServiceAudience.ServiceBusAudience);
+                    var authResult = await app.AcquireTokenForClient(new string[] { $"{audience}/.default" }).ExecuteAsync();
+                    return authResult.AccessToken;
 
-                return authResult.AccessToken;
-            };
+                });
+
+            await SendReceiveAsync(qClient);
         }
 
-        async Task UserPasswordCredentialScenario()
-        {
-            UserPasswordCredential userPasswordCredential = new UserPasswordCredential(
-                ConfigurationManager.AppSettings["userName"],
-                ConfigurationManager.AppSettings["password"]
-                );
-            MessagingFactorySettings messagingFactorySettings = new MessagingFactorySettings
-            {
-                TokenProvider = TokenProvider.CreateAadTokenProvider(
-                    new AuthenticationContext($"https://login.windows.net/{TenantId}"),
-                    ClientId,
-                    userPasswordCredential,
-                    ServiceAudience.ServiceBusAudience
-                ),
-                TransportType = TransportType.Amqp
-            };
-
-            await SendReceive(messagingFactorySettings);
-        }
-        
-        async Task ClientCredentialsCertScenario()
-        {
-            MessagingFactorySettings messagingFactorySettings = new MessagingFactorySettings
-            {
-                TokenProvider = TokenProvider.CreateAzureActiveDirectoryTokenProvider(AzureActiveDirectoryClientCertCallback),
-                TransportType = TransportType.Amqp
-            };
-
-            await SendReceive(messagingFactorySettings);
-        }
-
-        static AzureActiveDirectoryTokenProvider.AuthenticationCallback AzureActiveDirectoryClientCertCallback()
-        {
-
-            // Please fill out values below manually if the AAD tests should be run
-            string tenantId = ConfigurationManager.AppSettings["tenantId"];
-            string clientId = ConfigurationManager.AppSettings["clientId"];
-            string clientSecret = ConfigurationManager.AppSettings["clientSecret"];
-            if (string.IsNullOrEmpty(tenantId))
-            {
-                return null;
-            }
-            return async (audience, authority, state) =>
-            {
-                var authContext = new AuthenticationContext($"https://login.windows.net/{tenantId}", false);
-                var cc = new ClientCredential(clientId, clientSecret);
-                var authResult = await authContext.AcquireTokenAsync(ServiceBusAudience.ToString(), cc, ServiceAudience.ServiceBusAudience);
-                return authResult.AccessToken;
-            };
-
-        }
-
-        X509Certificate2 GetCertificate()
+        private X509Certificate2 GetCertificate()
         {
             List<StoreLocation> locations = new List<StoreLocation>
                 {
@@ -203,99 +257,27 @@ namespace MessagingSamples
             throw new ArgumentException($"A Certificate with Thumbprint '{ConfigurationManager.AppSettings["thumbPrint"]}' could not be located.");
         }
 
-        async Task SendReceive(MessagingFactorySettings messagingFactorySettings)
+        async Task ClientCredentialsCertScenario()
         {
-            MessagingFactory mf = MessagingFactory.Create($"sb://{ServiceBusNamespace}/", messagingFactorySettings);
+            X509Certificate2 certificate = GetCertificate();
 
-            this.receiveClient = mf.CreateQueueClient(QueueName, ReceiveMode.PeekLock);
-            this.InitializeReceiver();
-
-            this.sendClient = mf.CreateQueueClient(QueueName);
-            var sendTask = this.SendMessagesAsync();
-
-            Console.ReadKey();
-
-            // shut down the receiver, which will stop the OnMessageAsync loop
-            await this.receiveClient.CloseAsync();
-
-            // wait for send work to complete if required
-            await sendTask;
-
-            await this.sendClient.CloseAsync();
-        }
-
-        async Task SendMessagesAsync()
-        {
-            dynamic data = new[]
-            {
-                new {name = "Einstein", firstName = "Albert"},
-                new {name = "Heisenberg", firstName = "Werner"},
-                new {name = "Curie", firstName = "Marie"},
-                new {name = "Hawking", firstName = "Steven"},
-                new {name = "Newton", firstName = "Isaac"},
-                new {name = "Bohr", firstName = "Niels"},
-                new {name = "Faraday", firstName = "Michael"},
-                new {name = "Galilei", firstName = "Galileo"},
-                new {name = "Kepler", firstName = "Johannes"},
-                new {name = "Kopernikus", firstName = "Nikolaus"}
-            };
-
-
-            for (int i = 0; i < data.Length; i++)
-            {
-                var message = new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data[i]))))
+            var qClient = QueueClient.CreateWithAzureActiveDirectory(
+                new Uri($"sb://{ServiceBusNamespace}").ToString(),
+                QueueName,
+                async (audience, authority, state) =>
                 {
-                    ContentType = "application/json",
-                    Label = "Scientist",
-                    MessageId = i.ToString(),
-                    TimeToLive = TimeSpan.FromMinutes(2)
-                };
+                    IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(ClientId)
+                                    .WithAuthority(authority)
+                                    .WithCertificate(certificate)
+                                    .Build();
 
-                await this.sendClient.SendAsync(message);
-                lock (Console.Out)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Message sent: Id = {0}", message.MessageId);
-                    Console.ResetColor();
-                }
-            }
-        }
+                    var authResult = await app.AcquireTokenForClient(new string[] { $"{audience}/.default" }).ExecuteAsync();
+                    return authResult.AccessToken;
 
-        void InitializeReceiver()
-        {
-            // register the OnMessageAsync callback
-            this.receiveClient.OnMessageAsync(
-                async message =>
-                {
-                    if (message.Label != null &&
-                        message.ContentType != null &&
-                        message.Label.Equals("Scientist", StringComparison.InvariantCultureIgnoreCase) &&
-                        message.ContentType.Equals("application/json", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        var body = message.GetBody<Stream>();
+                });
 
-                        dynamic scientist = JsonConvert.DeserializeObject(new StreamReader(body, true).ReadToEnd());
+            await SendReceiveAsync(qClient);
 
-                        lock (Console.Out)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine(
-                                "\t\t\t\tMessage received: \n\t\t\t\t\t\tMessageId = {0}, \n\t\t\t\t\t\tSequenceNumber = {1}, \n\t\t\t\t\t\tEnqueuedTimeUtc = {2}," +
-                                "\n\t\t\t\t\t\tExpiresAtUtc = {5}, \n\t\t\t\t\t\tContentType = \"{3}\", \n\t\t\t\t\t\tSize = {4},  \n\t\t\t\t\t\tContent: [ firstName = {6}, name = {7} ]",
-                                message.MessageId,
-                                message.SequenceNumber,
-                                message.EnqueuedTimeUtc,
-                                message.ContentType,
-                                message.Size,
-                                message.ExpiresAtUtc,
-                                scientist.firstName,
-                                scientist.name);
-                            Console.ResetColor();
-                        }
-                    }
-                    await message.CompleteAsync();
-                },
-                new OnMessageOptions { AutoComplete = false, MaxConcurrentCalls = 1 });
         }
 
         public static int Main(string[] args)
@@ -317,3 +299,4 @@ namespace MessagingSamples
 
     }
 }
+
