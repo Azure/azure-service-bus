29d8ee799aceea72be1ae25152f71b28f2b377da
Sreedhar Pelluru <6722422+spelluru@users.noreply.github.com>
2024-12-09 23:37:23 -0500

    Updated to use latest packages

--- samples/DotNet/Azure.Messaging.ServiceBus/ServiceBusEventGridIntegrationV2/ReceiveMessagesFunctionApp/ReceiveMessagesOnEvent.cs
+++ samples/DotNet/Azure.Messaging.ServiceBus/ServiceBusEventGridIntegrationV2/ReceiveMessagesFunctionApp/ReceiveMessagesOnEvent.cs
@@ -1,28 +1,32 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
-//using Azure.Messaging.EventGrid;
-//using Azure.Messaging.EventGrid.SystemEvents;
-using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Extensions.Logging;
+using Azure.Messaging.ServiceBus;
+using Azure.Messaging.EventGrid;
+using System.Collections.Generic;
+using Azure.Messaging.EventGrid.SystemEvents;
 using Newtonsoft.Json.Linq;
+using Azure;
 
-namespace SBEventGridIntegrationV2
+namespace ReceiveMessagesFunctionApp
 {
     public static class ReceiveMessagesOnEvent
     {
-        const string ServiceBusConnectionString = "<SERCICE BUS NAMESPACE - CONNECTION STRING>";
+        const string ServiceBusConnectionString = "<SERVICE BUS NAMESPACE - CONNECTION STRING>";
 
         [FunctionName("EventGridTriggerFunction")]
-        public static void EventGridTriggerFunction([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
+        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
         {
             log.LogInformation("C# Event Grid trigger function processed a request.");
             log.LogInformation(eventGridEvent.Data.ToString());
 
-            var data = eventGridEvent.Data as JObject;
-            ServiceBusActiveMessagesAvailableWithNoListenersEventData eventData = data.ToObject<ServiceBusActiveMessagesAvailableWithNoListenersEventData>();            
-            
+            //var data = eventGridEvent.Data as JObject;
+            //ServiceBusActiveMessagesAvailableWithNoListenersEventData eventData = data.ToObject<ServiceBusActiveMessagesAvailableWithNoListenersEventData>();
+            ServiceBusActiveMessagesAvailableWithNoListenersEventData eventData = eventGridEvent.Data.ToObjectFromJson<ServiceBusActiveMessagesAvailableWithNoListenersEventData>();
+
             string topicName = eventData.TopicName;
             string subscriptionName = eventData.SubscriptionName;
 
@@ -46,15 +50,5 @@ namespace SBEventGridIntegrationV2
                 receiver.CompleteMessageAsync(receivedMessage).GetAwaiter().GetResult();
             }
         }
-    }
-
-    public class GridEvent
-    {
-        public string Id { get; set; }
-        public string EventType { get; set; }
-        public string Subject { get; set; }
-        public System.DateTime EventTime { get; set; }
-        public Dictionary<string, string> Data { get; set; }
-        public string Topic { get; set; }
     }
 }
